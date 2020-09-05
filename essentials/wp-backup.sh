f96a68422c1ea4b23855a6f97e3ec861092a65fb
Venkat Selvan <mailmevenkat25@gmail.com>
2020-09-05 19:54:41 +0530

    fix: issues with linux & tmp_dir creation fixed

--- essentials/wp-backup.sh
+++ essentials/wp-backup.sh
@@ -1,36 +1,40 @@
-#!/bin/sh
+#! /bin/bash
+# Author: Venkat Selvan
+# GitHub: https://github.com/venkatselvan/friday.sh
+
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 source "$DIR/utils.sh"
 
-echo "${LIGHTBLUE}platform: $PLATFORM \n"
+echo -e "${LIGHTBLUE}platform: $PLATFORM \n"
 
 if [[ ! -z "$help" ]]; then
-  echo "${GREEN}Example:"
-  echo "${LIGHTGREEN}./wp-backup.sh --site /var/www/website --db database --out output.zip"
+  echo -e "${GREEN}Example:"
+  echo -e "${LIGHTGREEN}./wp-backup.sh --site /var/www/website --db database --out output.zip"
   exit 1
 fi
 
-tmp_dir=$(mktemp -d -t $db)
+tmp_dir=$(mktemp -d -t "${db}_XXXXXXX")
 
-echo "${GREEN}Checking Prerequisite..."
+echo -e "${GREEN}Checking Prerequisite..."
 cmd_exit_if_not_available mysqldump "Please install mysqldump to run the script"
 cmd_install zip "brew install zip" "sudo apt-get install zip"
 
-echo "${GREEN}Zipping $site to a temp directory"
-echo sudo zip -r \"$tmp_dir/$db.zip\" $site
+echo -e "${GREEN}Zipping $site to a temp directory"
+sudo zip -r "$tmp_dir/$db.zip" $site
 
-echo "${GREEN}Exporting SQL to temp directory"
-echo "${LIGHTGREEN}Please enter your mysql username"
+echo -e "${GREEN}Exporting SQL to temp directory"
+echo -e "${LIGHTGREEN}Please enter your mysql username"
 read mysql_user
 
-echo "${LIGHTGREEN}Please enter your mysql password"
-echo mysqldump -u $mysql_user -p $db > \"$tmp_dir/$db.sql\"
+echo $tmp_dir
+echo -e "${LIGHTGREEN}Please enter your mysql password"
+sudo mysqldump -u $mysql_user -p $db > "$tmp_dir/$db.sql"
 
-echo "${GREEN}Creating $out from temp directory"
-echo sudo zip -r $out \"$tmp_dir/$db.zip\" \"$tmp_dir/$db.sql\"
+echo -e "${GREEN}Creating $out from temp directory"
+sudo zip -r $out "$tmp_dir/$db.zip" "$tmp_dir/$db.sql"
 
-echo "${GREEN}Cleaning up temp files"
+echo -e "${GREEN}Cleaning up temp files"
 rm -rf $tmp_dir
 
-echo "${LIGHTBLUE}Voila! We are done"
+echo -e "${LIGHTBLUE}Voila! We are done"
